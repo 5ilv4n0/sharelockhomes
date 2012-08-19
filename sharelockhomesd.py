49b706e790d3a97c69b08737e47b17a6567885cd
Silvano Wegener <silvano.wegener@googlemail.com>
2012-08-19 20:46:34 +0200

    logging verbessert

--- sharelockhomesd.py
+++ sharelockhomesd.py
@@ -5,7 +5,6 @@
 
 from lib.settings import *
 from lib.logging import log
-from lib.Sock import SockServer, ServerHandler
 import sys, json
 import lib.basic as basic
 
@@ -27,9 +26,6 @@ def main():
 
     logFile.writeToFile = configuration['logging']
     logFile.write('ShareLockHomes v' + VERSION + ' starting up')
-
-    server = SockServer(ServerHandler, configuration, logFile)
-    server.serve_forever()
 
 
 
