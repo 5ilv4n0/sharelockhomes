e0c72af20dff7da9f6616ea5ff1da0a41b700ae5
Silvano Wegener <silvano.wegener@googlemail.com>
2012-08-19 14:39:45 +0200

    add vim info

--- sharelockhomesd.py
+++ sharelockhomesd.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 #
-#  Copyright 2012 Silvano Wegener
+#  Copyright 2012 Silvano Wegener & Daniel Henschel
 
 
 
