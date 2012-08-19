4eba5f60615e1b4cb4ba0238867f1b3dac010264
Daniel Henschel <daniel@is-bored.de>
2012-08-19 15:05:18 +0200

    fix tabs

--- lib/logging.py
+++ lib/logging.py
@@ -10,26 +10,26 @@ class log(object):
         self.filePath = os.path.abspath(path)
         self.fileBuffer = open(self.filePath,'a')
         self.enabled = enabled
-		
-	def write(self, line):
-		if self.enabled:
-			dateTimeStamp = self.nowDateTimeStamp()
-			logLine = self.makeLogLine(dateTimeStamp, line)
-			self.fileBuffer.write(logLine)
-			self.fileBuffer.flush()
-		return self.enabled
-				
-	def nowDateTimeStamp(self):
-		dateTimeStamp = time.strftime("[%d.%m.%Y - %H:%M:%S]", time.localtime())
-		return dateTimeStamp
-		
-	def makeLogLine(self, dateTimeStamp, line):
-		logLine = []
-		logLine.append(dateTimeStamp)
-		logLine.append('->')
-		logLine.append(line)
-		logLine.append(os.linesep)
-		return ' '.join(logLine)
-		
-	def __del__(self):
-		self.fileBuffer.close()
+
+    def write(self, line):
+        if self.enabled:
+            dateTimeStamp = self.nowDateTimeStamp()
+            logLine = self.makeLogLine(dateTimeStamp, line)
+            self.fileBuffer.write(logLine)
+            self.fileBuffer.flush()
+        return self.enabled
+
+    def nowDateTimeStamp(self):
+        dateTimeStamp = time.strftime("[%d.%m.%Y - %H:%M:%S]", time.localtime())
+        return dateTimeStamp
+
+    def makeLogLine(self, dateTimeStamp, line):
+        logLine = []
+        logLine.append(dateTimeStamp)
+        logLine.append('->')
+        logLine.append(line)
+        logLine.append(os.linesep)
+        return ' '.join(logLine)
+
+    def __del__(self):
+        self.fileBuffer.close()
